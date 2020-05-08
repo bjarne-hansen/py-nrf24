df19591bcba150faabc916b4a5f70fceee41bb76
Bjarne Hansen <bjarne@conspicio.dk>
2020-05-08 17:47:34 +0200

    Released version 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("PyPI.md", "r") as fh:
 
 setuptools.setup(
     name="nrf24",
-    version="1.0.0",
+    version="1.0.1",
     keywords='nrf24l01 iot raspberry arduino',
     author="Bjarne Hansen",
     author_email="bjarne@conspicio.dk",
